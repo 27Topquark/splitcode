038b8b6e654da277633eb3bbbf633b9a265c0c64
27Topquark <abhinav.sharma27.as@gmail.com>
2016-10-06 13:46:31 -0400

    Add files via upload

--- Kalman_run.m
+++ Kalman_run.m
@@ -1,9 +1,20 @@
-%Kalman Data Preparation
+%Updated on October 4th 2016
+
+%% Preparing initial model
 clc
 clear all
 close all
-load Feet_position_allframes
 load model_init
+load Feet_position_allframes
+model_init_front = model_init;
+model_init_front.mu0 = [Front(1,1);Front(1,2)];
+model_init_hind = model_init;
+model_init_hind.mu0 = [Hind(1,1);Hind(1,2)];
+
+%% Kalman Data Preparation
+
+load Feet_position_allframes
+
 Hind = Hind';
 Front = Front';
 Hind_x = Hind(1,:);
@@ -11,8 +22,10 @@ Hind_y = Hind(2,:);
 Front_x = Front(1,:);
 Front_y = Front(2,:);
 
-Hind_new = [Hind_x(Hind_x~=0); Hind_y(Hind_y~=0)];
-Front_new = [Front_x(Front_x~=0); Front_y(Front_y~=0)];
+% Hind_new = [Hind_x(Hind_x~=0); Hind_y(Hind_y~=0)];
+% Front_new = [Front_x(Front_x~=0); Front_y(Front_y~=0)];
+Front_new = Front;
+Hind_new = Hind;
 
 %% Running Kalman parameter initialisation
 %% Running Kalman smoother
